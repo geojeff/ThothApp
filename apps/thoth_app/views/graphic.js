6e996086bf6c34a6b2309926baa5abd5410c65f8
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-11-30 06:51:50 -0600

    Enhanced circle drawing, with the addition of a function to draw circle icons based on recordType.

--- apps/thoth_app/views/graphic.js
+++ apps/thoth_app/views/graphic.js
@@ -5,46 +5,76 @@
 
 ThothApp.GraphicView = Sai.CanvasView.design({
 
-   renderCanvas: function(canvas, firstTime) {
-     if (firstTime) {
-       var i, c, x=10, y=10, yStep=20;
+  renderCanvas: function(canvas, firstTime) {
+    if (firstTime) {
+      this.addRecordIcons(canvas, ThothApp.Review, 10, 10, 20);
+      this.addRecordIcons(canvas, ThothApp.Version, 40, 10, 20);
+      this.addRecordIcons(canvas, ThothApp.Book, 70, 10, 20);
+      this.addRecordIcons(canvas, ThothApp.Author, 100, 10, 20);
+    }
+  },
 
-       for (i=0; i<ThothApp.loadedReviewCount; i++) {
-         c = canvas.circle(x, y, 10);
-         c.set('fill', '#ff0000');
-         c.set('id', 'review-%@'.fmt(i));
-         y += yStep;
-       }
+  addRecordIcons: function(canvas, recordType, xStart, yStart, yStep) {
+    var x=xStart, y=yStart, count=0, fixturesCount=0, recordTypeString='', recordTypeColor='ffffff';
 
-       x=40, y=10;
-       for (i=0; i<ThothApp.loadedVersionCount; i++) {
-         c = canvas.circle(x, y, 10);
-         c.set('fill', '#00ff00');
-         c.set('id', 'version-%@'.fmt(i));
-         y += yStep;
-       }
+    switch (recordType.toString()) {
+      case 'ThothApp.Review':
+        recordTypeString = 'review';
+        recordTypeColor = '#ff0000';
+        count = ThothApp.loadedReviewCount;
+        fixturesCount = ThothApp.Review.FIXTURES.get('length');
+        break;
+      case 'ThothApp.Version':
+        recordTypeString = 'version';
+        recordTypeColor = '#00ff00';
+        count = ThothApp.loadedVersionCount;
+        fixturesCount = ThothApp.Version.FIXTURES.get('length');
+        break;
+      case 'ThothApp.Book':
+        recordTypeString = 'book';
+        recordTypeColor = '#0000ff';
+        count = ThothApp.loadedBookCount;
+        fixturesCount = ThothApp.Book.FIXTURES.get('length');
+        break;
+      case 'ThothApp.Author':
+        recordTypeString = 'author';
+        recordTypeColor = '#00ffff';
+        count = ThothApp.loadedAuthorCount;
+        fixturesCount = ThothApp.Author.FIXTURES.get('length');
+        break;
+      default:
+        recordTypeString = 'none';
+        recordTypeColor = '#000000';
+        count = 0;
+        fixturesCount = 0;
+        break;
+    }
 
-       x=70, y=10;
-       for (i=0; i<ThothApp.loadedBookCount; i++) {
-         c = canvas.circle(x, y, 10);
-         c.set('fill', '#0000ff');
-         c.set('id', 'book-%@'.fmt(i));
-         y += yStep;
-       }
+    var i, c;
+    if (count > 0) {
+      for (i=0; i<count; i++) {
+        c = canvas.circle(x, y, 10);
+        c.set('fill', recordTypeColor);
+        c.set('id', '%@-%@'.fmt(recordTypeString, i));
+        y += yStep;
+      }
+    } else {
+      for (i=0; i<fixturesCount; i++) {
+        c = canvas.circle(x, y, 10);
+        c.set('fill', '#ffffff');
+        c.set('fill-opacity', 0.5);
+        c.set('stroke', '#000000');
+        c.set('stroke-width', 1);
+        c.set('stroke-dasharray', '-..');
+        //c.set('stroke-dasharray', ['', '-', '.', '-.', '-..', '. ', '- ', '--', '- .', '--.', '--..']);
+        c.set('id', '%@-%@'.fmt(recordTypeString, i));
+        y += yStep;
+      }
+    }
+  },
 
-       x=100, y=10;
-       for (i=0; i<ThothApp.loadedAuthorCount; i++) {
-         c = canvas.circle(x, y, 10);
-         c.set('fill', '#00ffff');
-         c.set('id', 'author-%@'.fmt(i));
-         y += yStep;
-       }
-
-     }
-   },
-
-   mouseDown: function(evt) {
-     console.log(evt.target);
-   }
+  mouseDown: function(evt) {
+    console.log(evt.target);
+  }
 
  });
